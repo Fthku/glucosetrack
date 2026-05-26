da4fc9eececd5aa6e90bef68df8168ff0ad9db58
Fthku <drorop@gmail.com>
2026-05-27 02:10:50 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+const CACHE = 'glucosetrack-v1';
+const ASSETS = [
+  '/glucosetrack/',
+  '/glucosetrack/index.html',
+  '/glucosetrack/manifest.json',
+  '/glucosetrack/icon-192.png',
+  '/glucosetrack/icon-512.png'
+];
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); self.skipWaiting(); });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))); self.clients.claim(); });
+self.addEventListener('fetch', e => { e.respondWith(fetch(e.request).then(res => { const clone = res.clone(); caches.open(CACHE).then(c => c.put(e.request, clone)); return res; }).catch(() => caches.match(e.request))); });
